cafdb72d0edefee1f3780edea9d8cf4a03436fef
Ivan Grakov <ivan.grakov@gmail.com>
2026-05-12 10:04:05 +0300

    Rename app

--- src/permissions_management_demo/asgi.py
+++ src/permissions_management_demo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for effective_mobile_test project.
+ASGI config for permissions_management_demo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "effective_mobile_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "permissions_management_demo.settings")
 
 application = get_asgi_application()
